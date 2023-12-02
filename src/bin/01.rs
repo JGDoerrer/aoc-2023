e0547a466833a8344ee0a449924db53e15af0dfa
JGDoerrer <jg.doerrer@gmail.com>
2023-12-02 12:10:37 +0100

    fix test day 1 part 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -93,7 +93,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(142));
+        assert_eq!(result, Some(209));
     }
 
     #[test]
