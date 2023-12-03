832b50c83b5ccbc10ff5c83302389c4c39170748
JGDoerrer <jg.doerrer@gmail.com>
2023-12-03 11:11:41 +0100

    optimize day 3

--- README.md
+++ README.md
@@ -19,11 +19,11 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 
 | Day | Part 1 | Part 2 |
 | :---: | :---: | :---:  |
-| [Day 1](./src/bin/01.rs) | `55.1µs` | `1.1ms` |
-| [Day 2](./src/bin/02.rs) | `29.5µs` | `51.3µs` |
-| [Day 3](./src/bin/03.rs) | `34.9ms` | `41.0ms` |
+| [Day 1](./src/bin/01.rs) | `55.4µs` | `1.1ms` |
+| [Day 2](./src/bin/02.rs) | `30.5µs` | `50.0µs` |
+| [Day 3](./src/bin/03.rs) | `4.8ms` | `5.2ms` |
 
-**Total: 77.14ms**
+**Total: 11.24ms**
 <!--- benchmarking table --->
 
 ---
--- src/bin/03.rs
+++ src/bin/03.rs
@@ -3,47 +3,47 @@ use std::collections::HashMap;
 advent_of_code::solution!(3);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let mut numbers = vec![];
+    let mut sum = 0;
+    let lines = input.lines().count();
+    let line_len = input.lines().next().unwrap().len();
 
     for (i, line) in input.lines().enumerate() {
         let mut j = 0;
-        while j < line.len() {
+        'num_loop: while j < line.len() {
             if line.chars().nth(j).unwrap().is_ascii_digit() {
                 let num: String = line
                     .chars()
                     .skip(j)
                     .take_while(|c| c.is_ascii_digit())
                     .collect();
-                let start_j = j;
-                j += num.len();
+                let len = num.len();
+
                 let num: u32 = num.parse().unwrap();
-                numbers.push((i, start_j, num));
+
+                for di in -1isize..=1 {
+                    let i = (i as isize + di).clamp(0, lines as isize - 1) as usize;
+
+                    let start_j = j.saturating_sub(1);
+                    let end_j = (j + len + 1).min(line_len);
+
+                    if input
+                        .lines()
+                        .nth(i)
+                        .unwrap()
+                        .chars()
+                        .take(end_j)
+                        .skip(start_j)
+                        .any(|c| c != '.' && !c.is_ascii_digit())
+                    {
+                        sum += num;
+                        j += len;
+                        continue 'num_loop;
+                    }
+                }
+
+                j += len;
             } else {
                 j += 1;
-            }
-        }
-    }
-
-    let mut sum = 0;
-    'num_loop: for (i, j, num) in numbers {
-        let len = num.ilog10() + 1;
-
-        for di in -1isize..=1 {
-            let i = (i as isize + di).clamp(0, input.lines().count() as isize) as usize;
-
-            for dj in -1isize..=len as isize {
-                let j = (j as isize + dj).clamp(0, input.lines().nth(j).unwrap().len() as isize)
-                    as usize;
-
-                if input
-                    .lines()
-                    .nth(i)
-                    .and_then(|line| line.chars().nth(j).map(|c| c != '.' && !c.is_ascii_digit()))
-                    .unwrap_or(false)
-                {
-                    sum += num;
-                    continue 'num_loop;
-                }
             }
         }
     }
@@ -52,51 +52,53 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let mut numbers = vec![];
+    let mut gears: HashMap<(usize, usize), Vec<u32>> = HashMap::new();
+    let lines = input.lines().count();
+    let line_len = input.lines().next().unwrap().len();
 
     for (i, line) in input.lines().enumerate() {
         let mut j = 0;
-        while j < line.len() {
+        'num_loop: while j < line.len() {
             if line.chars().nth(j).unwrap().is_ascii_digit() {
                 let num: String = line
                     .chars()
                     .skip(j)
                     .take_while(|c| c.is_ascii_digit())
                     .collect();
-                let start_j = j;
-                j += num.len();
+                let len = num.len();
+
                 let num: u32 = num.parse().unwrap();
-                numbers.push((i, start_j, num));
+
+                for di in -1isize..=1 {
+                    let i = (i as isize + di).clamp(0, lines as isize - 1) as usize;
+
+                    let start_j = j.saturating_sub(1);
+                    let end_j = (j + len + 1).min(line_len);
+
+                    if let Some((char_j, _)) = input
+                        .lines()
+                        .nth(i)
+                        .unwrap()
+                        .chars()
+                        .enumerate()
+                        .take(end_j)
+                        .skip(start_j)
+                        .filter(|(_, c)| *c == '*')
+                        .next()
+                    {
+                        if let Some(gear) = gears.get_mut(&(i, char_j)) {
+                            gear.push(num);
+                        } else {
+                            gears.insert((i, char_j), vec![num]);
+                        }
+                        j += len;
+                        continue 'num_loop;
+                    }
+                }
+
+                j += len;
             } else {
                 j += 1;
-            }
-        }
-    }
-
-    let mut gears: HashMap<(usize, usize), Vec<u32>> = HashMap::new();
-    'num_loop: for (i, j, num) in numbers {
-        let len = num.ilog10() + 1;
-
-        for di in -1isize..=1 {
-            let i = (i as isize + di).clamp(0, input.lines().count() as isize) as usize;
-
-            for dj in -1isize..=len as isize {
-                let j = (j as isize + dj).clamp(0, input.lines().nth(j).unwrap().len() as isize)
-                    as usize;
-
-                if input
-                    .lines()
-                    .nth(i)
-                    .and_then(|line| line.chars().nth(j).map(|c| c == '*'))
-                    .unwrap_or(false)
-                {
-                    if let Some(gear) = gears.get_mut(&(i, j)) {
-                        gear.push(num);
-                    } else {
-                        gears.insert((i, j), vec![num]);
-                    }
-                    continue 'num_loop;
-                }
             }
         }
     }
