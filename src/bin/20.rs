912651c910994b80826312d2d1e1abf41dbe62cd
JGDoerrer <jg.doerrer@gmail.com>
2024-12-02 10:17:18 +0100

    remove 20.2 test

--- src/bin/20.rs
+++ src/bin/20.rs
@@ -252,10 +252,4 @@ mod tests {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(11687500));
     }
-
-    #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
-    }
 }
